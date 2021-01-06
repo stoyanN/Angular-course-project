edbee4cf06521b0e1a9056598cfb90ccdd39f4d7
StoyanN <stoyannenkov@abv.bg>
2021-01-06 15:30:05 +0200

    Backendless token updates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   backendless: {
-    APP_ID: '99D0443C-0C96-0827-FFA4-82CA89294E00',
-    API_KEY: 'A876CDFE-6301-49CF-A4F3-EBE66959D2C0'
+    APP_ID: 'A2EE51E2-94DD-0CD0-FFA8-C98E74943B00',
+    API_KEY: '61EF6831-9872-4168-8377-45EDA2927462'
   }
 };
 
